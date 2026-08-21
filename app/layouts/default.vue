dc246f4c869d7089b08973978c2ef98a142d39bd
Ali Sunjaya <ali.sunjayaa@gmail.com>
2026-08-21 14:32:06 +0700

    chore: add ci

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,65 +1,72 @@
 <script setup lang="ts">
-import type { NavigationMenuItem } from "@nuxt/ui";
-const route = useRoute();
+import type { NavigationMenuItem } from '@nuxt/ui'
 
 const items = computed<NavigationMenuItem[]>(() => [
   {
-    label: "Home",
-    to: "/",
+    label: 'Home',
+    to: '/',
   },
-]);
+])
 </script>
 
 <template>
-  <UHeader>
-    <template #left>
-      <NuxtLink to="/docs">
-        <Logo class="h-6 w-auto" />
-      </NuxtLink>
-    </template>
+  <div>
+    <UHeader>
+      <template #left>
+        <NuxtLink to="/docs">
+          <Logo class="h-6 w-auto" />
+        </NuxtLink>
+      </template>
 
-    <UNavigationMenu :items="items" variant="link" />
-
-    <template #body>
-      <UNavigationMenu :items="items" orientation="vertical" />
-    </template>
-
-    <template #right>
-      <UColorModeButton />
-    </template>
-  </UHeader>
-
-  <UMain>
-    <slot />
-  </UMain>
-
-  <USeparator />
-
-  <UFooter>
-    <template #left>
-      <p class="text-sm text-muted">
-        © {{ new Date().getFullYear() }} Ali Sunjaya
-      </p>
-    </template>
-
-    <template #right>
-      <UButton
-        icon="i-simple-icons-github"
-        to="https://github.com/sunjayaali"
-        target="_blank"
-        variant="ghost"
-        color="neutral"
-        aria-label="GitHub"
+      <UNavigationMenu
+        :items="items"
+        variant="link"
       />
 
-      <UButton
-        icon="i-simple-icons-linkedin"
-        to="https://www.linkedin.com/in/ali-sunjaya-6b8b54141/"
-        target="_blank"
-        variant="ghost"
-        color="neutral"
-        aria-label="LinkedIn"
-      />
-    </template>
-  </UFooter>
+      <template #body>
+        <UNavigationMenu
+          :items="items"
+          orientation="vertical"
+        />
+      </template>
+
+      <template #right>
+        <UColorModeButton />
+      </template>
+    </UHeader>
+
+    <UMain>
+      <slot />
+    </UMain>
+
+    <USeparator />
+
+    <UFooter>
+      <template #left>
+        <p class="text-sm text-muted">
+          © {{ new Date().getFullYear() }} Ali Sunjaya
+        </p>
+      </template>
+
+      <template #right>
+        <UButton
+          icon="i-simple-icons-github"
+          to="https://github.com/sunjayaali"
+          target="_blank"
+          variant="ghost"
+          color="neutral"
+          aria-label="GitHub"
+        />
+
+        <UButton
+          icon="i-simple-icons-linkedin"
+          to="https://www.linkedin.com/in/ali-sunjaya-6b8b54141/"
+          target="_blank"
+          variant="ghost"
+          color="neutral"
+          aria-label="LinkedIn"
+        />
+      </template>
+    </UFooter>
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui", "@nuxt/content"],
-  css: ["~/assets/css/main.css"],
-  compatibilityDate: "2025-07-15",
+  modules: ['@nuxt/ui', '@nuxt/content', '@nuxt/eslint'],
   devtools: { enabled: true },
-});
+  css: ['~/assets/css/main.css'],
+  compatibilityDate: '2025-07-15',
+  typescript: {
+    typeCheck: true,
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+})
