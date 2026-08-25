3c99694dd4151bbd0f6f2b452ac74b669dc3f9c4
Ali Sunjaya <ali.sunjayaa@gmail.com>
2026-08-25 11:14:57 +0700

    feat: add InstagramReel component and multiple cycling assets

---
+++ app/components/content/InstagramReel.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps<{
+  url: string
+}>()
+
+const { onLoaded } = useScript({
+  src: 'https://www.instagram.com/embed.js',
+})
+
+onMounted(() => {
+  onLoaded(() => {
+    window.instgrm?.Embeds?.process?.()
+  })
+})
+</script>
+
+<template>
+  <div class="flex w-full justify-center">
+    <blockquote
+      class="instagram-media"
+      :data-instgrm-permalink="props.url"
+      data-instgrm-version="14"
+    />
+  </div>
+</template>
